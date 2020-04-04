cdc3a9f623fb9163527afece0b448abb2386cff9
Brendan Lewis <blewis@na-alii.com>
2020-04-04 00:02:24 -1000

    Added views code and templates

--- campaigns/models.py
+++ campaigns/models.py
@@ -1,9 +1,16 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Campaign(models.Model):
     title = models.CharField(max_length=30)
     start_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.title
+    def was_published_recently(self):
+        return self.start_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Session(models.Model):
@@ -11,4 +18,6 @@ class Session(models.Model):
     number = models.IntegerField(default=0)
     date = models.DateTimeField('date published')
     description = models.CharField(max_length=300)
+    def __int__(self):
+        return self.number
 
--- campaigns/views.py
+++ campaigns/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+
+from .models import Campaign
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the campaigns index.")
+	latest_campaigns_list = Campaign.objects.order_by('-start_date')
+	context = {
+		'latest_campaigns_list' : latest_campaigns_list
+	}
+	return render(request, 'campaigns/index.html',context)
+
+def campaignDetail(request, campaign_id):
+		campaign = get_object_or_404(Campaign, pk=campaign_id)
+		return render(request, 'campaigns/detail.html', {'campaign': campaign})
+
+def sessionsView(request, campaign_id):
+	response = "You're looking at the sessions for the campaign %s."	
+	return HttpResponse(response % campaign_id)
+
+def sessionDetail(request, campaign_id, session_id):
+	response = "You're looking at session %s."
+	return HttpResponse(response % session_id)
+ 
